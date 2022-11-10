e54ec55bb4ee960b7de1f95b9505a740e63afd4a
Durga prasad <Durga prasad@LAPTOP-LN9V9DGH>
2022-11-10 22:13:36 +0530

    jenkins demo

---
+++ src/main/java/acoecsea/dpdemojenkins/App.java
@@ -0,0 +1,13 @@
+package acoecsea.dpdemojenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "WELCOME TO DEVOPS" );
+    }
+}
